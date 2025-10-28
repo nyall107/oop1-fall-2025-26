b5dba8b1ea9629ad870933ff1166cd6b08116ddb
nyall107 <aznyall410@gmail.com>
2025-10-28 17:26:20 +0600

    25-62020-2

---
+++ Nyall.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Nyall {
+     public static void main(String[] args) {
+       
+       Scanner Sc = new Scanner(System.in);
+       int price = 250;
+       float discount = 0.15f;
+       
+       float Final = discount * price;
+
+       System.out.println(Final);
+    }
+}
+
