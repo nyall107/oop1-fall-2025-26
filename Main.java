cb19416c15fc0f2cbbd48f4d98cd13bb36368207
nyall107 <aznyall410@gmail.com>
2025-10-28 17:10:10 +0600

    52-62020-2

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       
+       Scanner Sc = new Scanner(System.in);
+       int price = 250;
+       float discount = 0.15f;
+       
+       float Final = discount * price;
+
+       System.out.println(Final);
+    }
+}
