6abb1c32e9564a56dba9c6c8f9744b9ebb7c8cce
nyall107 <aznyall410@gmail.com>
2025-10-29 21:32:40 +0600

    25-62020-2

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-       
-       Scanner Sc = new Scanner(System.in);
-       int price = 250;
-       float discount = 0.15f;
-       
-       float Final = discount * price;
+        Scanner scanner = new Scanner(System.in);
 
-       System.out.println(Final);
+        System.out.print("Enter the first integer: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter the second integer: ");
+        int b = scanner.nextInt();
+
+        a = a + b;
+        b = a - b;
+        a = a - b; 
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
     }
 }
--- Main2.java
+++ Main2.java
@@ -1,1 +1,12 @@
-*
+import java.util.*;
+public class Main2 {
+public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+
+  System.out.print("Enter Celcius : ");
+  int C = sc.nextInt();
+
+  int F =9/5*C+32;
+  System.out.println("Fahrenheit: "+F);
+ }    
+}
